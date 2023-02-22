14fa84b3ad78de4e06e4ae72be1f9f24fec38f32
KonggMinhh <hoangkongminh2001@gmail.com>
2023-02-22 21:50:21 +0700

    index-js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,35 @@
 "use strict";
 import { sidebar } from "./sidebar.js";
 import { api_key, imageBaseURL, fetchDataFromServer } from "./api.js";
+import { createMovieCard } from "./movie-card.js";
 
 const pageContent = document.querySelector("[page-content]");
 
 sidebar();
 
+const homePageSections = [
+    {
+        title: "Upcoming Movies",
+        path: "/movie/upcoming",
+    },
+    {
+        title: "Today's Trending Movies",
+        path: "/trending/movie/week",
+    },
+    {
+        title: "Top Rated Movies",
+        path: "/movie/top_rated",
+    },
+];
+
 const genreList = {
     asString(genreIdList) {
         let newGenreList = [];
-        for(const genreId of genreIdList) {
-            this[genreId]  && newGenreList.push(this[genreId]);
+        for (const genreId of genreIdList) {
+            this[genreId] && newGenreList.push(this[genreId]);
         }
         return newGenreList.join(", ");
-    }
-
+    },
 };
 
 fetchDataFromServer(
@@ -71,7 +86,9 @@ const heroBanner = function ({ results: movieList }) {
             <h2 class="heading">${title}</h2>
             <div class="meta-list">
                 <div class="meta-item">${release_date.split("-")[0]}</div>
-                <div class="meta-item card-badge">${vote_average.toFixed(1)}</div>
+                <div class="meta-item card-badge">${vote_average.toFixed(
+                    1
+                )}</div>
             </div>
             <p class="genre">${genreList.asString(genre_ids)}</p>
             <p class="banner-text">${overview}
@@ -88,14 +105,14 @@ const heroBanner = function ({ results: movieList }) {
             </a>
         </div>`;
 
-        banner.querySelector(".banner-slider").appendChild(sliderItem)
-        
+        banner.querySelector(".banner-slider").appendChild(sliderItem);
+
         const controlItem = document.createElement("button");
         controlItem.classList.add("poster-box", "slider-item");
-        controlItem.setAttribute("slider-control", `${controlItemIndex}`)
+        controlItem.setAttribute("slider-control", `${controlItemIndex}`);
 
         controlItemIndex++;
-        
+
         controlItem.innerHTML = `
         <img
                     class="img-cover"
@@ -104,34 +121,93 @@ const heroBanner = function ({ results: movieList }) {
                     loading="lazy"
                     draggable="false"
         />
-        `
+        `;
         banner.querySelector(".control-inner").appendChild(controlItem);
     }
 
     pageContent.appendChild(banner);
     addHeroSlide();
 
+    for (const { title, path } of homePageSections) {
+        fetchDataFromServer(
+            `https://api.themoviedb.org/3${path}?api_key=${api_key}&page=1`,
+            createMovieList,
+            title
+        );
+    }
 };
 
 const addHeroSlide = function () {
-    const sliderItems = document.querySelectorAll('[slider-item]');
-    const sliderControls = document.querySelectorAll('[slider-control]');
+    const sliderItems = document.querySelectorAll("[slider-item]");
+    const sliderControls = document.querySelectorAll("[slider-control]");
 
     let lastSliderItem = sliderItems[0];
     let lastSliderControl = sliderControls[0];
 
-    lastSliderItem.classList.add('active');
-    lastSliderControl.classList.add('active');
+    lastSliderItem.classList.add("active");
+    lastSliderControl.classList.add("active");
 
     const sliderStart = function () {
-        lastSliderItem.classList.remove('active');
-        lastSliderControl.classList.remove('active');
-        sliderItems[Number(this.getAttribute('slider-control'))].classList.add('active');
-        this.classList.add('active');
+        lastSliderItem.classList.remove("active");
+        lastSliderControl.classList.remove("active");
+        sliderItems[Number(this.getAttribute("slider-control"))].classList.add(
+            "active"
+        );
+        this.classList.add("active");
 
-        lastSliderItem = sliderItems[Number(this.getAttribute('slider-control'))];
+        lastSliderItem =
+            sliderItems[Number(this.getAttribute("slider-control"))];
         lastSliderControl = this;
+    };
+    addEventOnElements(sliderControls, "click", sliderStart);
+};
 
+const createMovieList = function ({ results: movieList }, title) {
+    const movieListElem = document.createElement("section");
+    movieListElem.classList.add("movie-list");
+    movieListElem.ariaLabel = `${title}`;
+    movieListElem.innerHTML = `
+                <div class="title-wrapper">
+                    <h3 class="title-large">${title}</h3>
+                </div>
+                <div class="slider-list">
+                    <div class="slider-inner">
+                        <div class="movie-card">
+                            <figure class="poster-box card-banner">
+                                <img
+                                    src="./img/slider-control.jpg"
+                                    alt="Puss in Boots: The Last Wish"
+                                    class="img-cover"
+                                />
+                            </figure>
+                            <h4 class="title">Puss in Boots: The Last Wish</h4>
+
+                            <div class="meta-list">
+                                <div class="meta-item">
+                                    <img
+                                        src="./icons/star.png"
+                                        width="20"
+                                        height="20"
+                                        alt="rating"
+                                    />
+
+                                    <span class="span">8.4</span>
+                                </div>
+
+                                <div class="card-badge">2022</div>
+                            </div>
+
+                            <a
+                                href="./detail.html"
+                                title="Puss in Boots: The Last Wish"
+                                class="card-btn"
+                            ></a>
+                        </div>                      
+                    </div>
+                </div>
+    `;
+
+    for (const movie of movieList) {
+        const movieCard = createMovieCard(movie);
     }
-    addEventOnElements(sliderControls, 'click', sliderStart)
-}
+};
